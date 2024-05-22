540c2ca6017d1e098cb092f241e5d3969fac4dda
ghosterecom <111013542+ghosterecom@users.noreply.github.com>
2024-05-22 15:04:58 -0300

    2205

--- main.js
+++ main.js
@@ -7,13 +7,27 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
     if(tamanhoSenha > 1){
-tamanhoSenha = tamanhoSenha - 1;
+//tamanhoSenha = tamanhoSenha - 1;
     }
 numeroSenha.textContent = tamanhoSenha;
+gerasenha();
 }
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
-    tamanhoSenha = tamanhoSenha + 1;
+    //tamanhoSenha = tamanhoSenha + 1;
     }
 numeroSenha.textContent = tamanhoSenha;
+gerasenha();
 }
+// codigo omitido
+const camposenha = document.querySelector( '#campo-senha');
+const letrasmaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function gerasenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++){
+    let numeroaleatorio = Math.random()*letrasmaiusculas.length;
+    numeroaleatorio = math.floor(numeroaleatorio);
+    senha = senha + letrasmaiusculas[numeroaleatorio];
+    }
+}
+camposenha.value = senha;
